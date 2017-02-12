e8dd07deac75299af758d4f1d6506c0540f65830
BeatrixSzekeres <rixi1125@gmail.com>
2017-02-12 15:57:42 +0100

    ce gomb is mükődik

--- hf1/Form1.Designer.cs
+++ hf1/Form1.Designer.cs
@@ -65,6 +65,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "CE";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -74,6 +75,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "C";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
--- hf1/Form1.cs
+++ hf1/Form1.cs
@@ -76,5 +76,16 @@ namespace hf1
             
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            eredmeny = 0;
+        }
     }
 }
