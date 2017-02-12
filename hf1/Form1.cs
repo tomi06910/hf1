b2d63ff92f4b8ec5ff9dc4e99a1f10d15eecfc76
BeatrixSzekeres <rixi1125@gmail.com>
2017-02-12 15:49:32 +0100

    hozzáadtam a gombokat és ki tudja írni a vizuális felületre

--- hf1/Form1.cs
+++ hf1/Form1.cs
@@ -12,9 +12,69 @@ namespace hf1
 {
     public partial class Form1 : Form
     {
+        double egyikszam;
+        double masikszam;
+        double eredmeny = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+
         }
     }
 }
