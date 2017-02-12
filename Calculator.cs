bf68703657cf83ac4372198878b0aea8e6efff10
Felhasználó <Felhasznl@PCUSER>
2017-02-12 15:56:57 +0100

    kész a calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1() { }
+
+
+
+        public static int Osszeadas(int egyikszam, int masikszam)
+    {
+        return egyikszam + masikszam;
+    }
+    public static int Kivonas(int egyikszam, int masikszam)
+    {
+        return egyikszam + masikszam;
+    }
+    public static int Szorzas(int egyikszam, int masikszam)
+    {
+        return egyikszam + masikszam;
+    }
+    public static int Osztas(int egyikszam, int masikszam)
+    {
+        return egyikszam + masikszam;
+    }
+    public static int C(int egyikszam, int masikszam)
+    {
+        return egyikszam + masikszam;
+    }
+    public static int CE(int egyikszam, int masikszam)
+    {
+        return egyikszam + masikszam;
+    }
+
+}
